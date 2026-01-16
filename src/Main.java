4365d13933d93e4899f51858e6e54ca4f6bab62a
Johnathan McAllister <doorman.jm@gmail.com>
2026-01-15 23:18:32 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,7 @@ public class Main {
 
     public final static Scanner input = new Scanner(System.in);
 
-    /**
-     * Requests value from user calls the recursive function fib, and then displays the results
-     * @param args
-     */
+
     static void main(String[] args) {
 
         int value;
